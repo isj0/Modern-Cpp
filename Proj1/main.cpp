21e29209169cd0a734afc948a81e191466e8dc29
isj <iqbal.singh23@gmail.com>
2025-03-04 16:34:22 -0800

    first commit

---
+++ Proj1/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Hello from Project 1";
+
+    return 0;
+}
---
+++ Proj2/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Hello from Project 2" << endl;
+    cin >> n;
+    cout << n << " * 3 == " << n * 3 << endl;
+    return 0;
+}
