43637acf98ac4502ba0c069a4a9c54b874bf0052
isj <iqbal.singh23@gmail.com>
2025-05-12 20:35:50 -0400

    new examples on for loop added.

--- 04_Program Control/ForLoop.cpp
+++ 04_Program Control/ForLoop.cpp
@@ -8,13 +8,13 @@ using namespace std;
 
 int main()
 {
-    /*for (int i{ 1 }; i <= 20; ++i) {
-        cout << i << endl;
-    }*/
+    // for (int i{ 1 }; i <= 20; ++i) {
+    //     cout << i << endl;
+    // }
 
-    /*for (int i{ 1 }; i <= 10; i += 2) {
-        cout << i << endl;
-    }*/
+    // for (int i{ 1 }; i <= 10; i += 2) {
+    //     cout << i << endl;
+    // }
 
     // for (int i{ 10 }; i > 0; --i) {
     //     cout << i << endl;
@@ -25,22 +25,23 @@ int main()
     // }
 
     // vector <int> nums {10, 20, 30, 40, 50};
-
+    //
     // for (int i{0}; i < nums.size(); ++i) {
     //     cout << nums[i] << endl;
     // }
 
-    int sum {0};
-    
-    for (int i{1}; i< 15; ++i) {
-        if (i / 2 != 0) {
-            sum += i;
+    // int sum {0};
+    //
+    // for (int i{1}; i< 15; ++i) {
+    //     if (i / 2 != 0) {
+    //         sum += i;
+    //
+    //         cout << i << endl;
+    //     }
+    //
+    // }
+    // cout << sum << endl;
 
-            cout << i << endl;
-        }
-
-    }
-    cout << sum << endl;
 
 
     return 0;
